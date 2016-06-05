33dcfdac0521437b1a34e5ff6da52c0a3e041a44
Maetz007 <maetz007@yahoo.com>
2016-06-05 10:49:58 -0500

    completed kinda hard and hard

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+$(document).ready( function() {
 
 var infoData;
-var temoStudent;
+var dataLength;
+var tempStudent;
 var studentCounter = 0;
 var tempCounter = 0;
 
@@ -11,9 +12,10 @@ var tempCounter = 0;
       url: 'https://raw.githubusercontent.com/devjanaprime/2.4-jQueryAjaxJSON/master/students.json',
       dataType: 'json',
       success: function(data){
-        console.log('AJAX loaded');
+        console.log('AJAX loaded'); // confirms info has been loaded
         studentInfo(data, studentCounter); // loads page with starting info from 0
-        infoData = data;
+        infoData = data; // stores the data globally
+        dataLength = data.students.length - 1; // sets a dynamic number for later use should the array size change
         }, // end success
       statusCode: {
         404: function(){
@@ -23,6 +25,7 @@ var tempCounter = 0;
     }); // end ajax  object
   });
 
+// first time call sets page with first student in array. subsequent calls cycle through student array and displays
   var studentInfo = function(info, num){
     var stuInfo = info.students;
     var first = stuInfo[num].first_name;
@@ -34,29 +37,30 @@ var tempCounter = 0;
     var countDisplay = "#" + stuNum + "/20";
 
     var newDiv = document.createElement("div");
-    newDiv.className = "studentDiv";
 
-    var countDiv = document.createElement("p");
-    countDiv.className = "countDiv";
-    countDiv.textContent = countDisplay;
-    newDiv.appendChild(countDiv);
-    $("body").append(newDiv);
-
-    var student = document.createElement("p");
+    var student = document.createElement("div"); // creates new student to display on page and appends to div
     student.className = "newStudent";
     student.textContent = display;
     newDiv.appendChild(student);
     $("body").append(newDiv);
-    tempCounter = countDiv;
-    tempStudent = student;
+
+    var countDiv = document.createElement("div"); // creates updated counter to display on page and appends to student
+    countDiv.className = "countDiv";
+    countDiv.textContent = countDisplay;
+    student.appendChild(countDiv);
+    $("body").append(newDiv);
+
+    tempCounter = countDiv; // creates a global element that can be removed later
+    tempStudent = student; // ditto
     };
 
-  $("#prevBtn").click(function(){
+  $("#prevBtn").click( function(){
     studentCounter--;
-    tempStudent.remove();
-    tempCounter.remove();
+    tempStudent.remove(); // removes previous element to be replaced by new student
+    tempCounter.remove(); // ditto except replaces counter display
+    // checks beginning of array an wraps to top of array
       if(studentCounter < 0){
-        studentCounter = 19;
+        studentCounter = dataLength;
         studentInfo(infoData, studentCounter);
       }
       else {
@@ -64,11 +68,12 @@ var tempCounter = 0;
         }
   }); // end PREV
 
-  $("#nextBtn").click(function(){
+  $("#nextBtn").click( function(){
     studentCounter++;
     tempStudent.remove();
     tempCounter.remove();
-      if(studentCounter > 19){
+    // checks end of array and wraps down to bottom of array
+      if(studentCounter > dataLength){
         studentCounter = 0;
         studentInfo(infoData, studentCounter);
       }
@@ -76,4 +81,12 @@ var tempCounter = 0;
         studentInfo(infoData, studentCounter);
         }
   }); // end NEXT
+
+  $("#selectBtn").on("change", function(){
+    studentCounter = parseInt($(this).val());
+    tempStudent.remove();
+    tempCounter.remove();
+    studentInfo(infoData, studentCounter);
+  }); // end select button
+
 }); // document ready
